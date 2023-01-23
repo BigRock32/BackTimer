b18abc452d3b6aca76f6ccb72838fbee7f6bea66
BigRock32 <vasya.samarin.00@mail.ru>
2023-01-23 21:12:46 +0800

    new version

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ function counts() {
    let minutes = Math.floor(gap / 1000 / 60) % 60
    let seconds = Math.floor(gap / 1000) % 60
 
-   if (gap < 0){
+   if (gap < 0) {
       
    }
 
-      document.getElementById('d').innerText = days
+   document.getElementById('d').innerText = days
    document.getElementById('h').innerText = hours
    document.getElementById('m').innerText = minutes
    document.getElementById('s').innerText = seconds
 
-   console.log(days);
+   if (seconds < 10) {
+      document.getElementById('s').innerText = '0' + seconds
+   }
 }
 
 setInterval(counts, 1000)
