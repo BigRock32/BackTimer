0aeec858169a561ab2b5758513c332e9c90762c2
BigRock32 <vasya.samarin.00@mail.ru>
2023-01-23 20:36:49 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let date = new Date('February 14 2023 12:40:00')
+
+function counts() {
+   let now = new Date()
+   gap = date - now
+
+   let days = Math.floor(gap / 1000 / 60 / 60 / 24)
+   let hours = Math.floor(gap / 1000 / 60 / 60) % 24
+   let minutes = Math.floor(gap / 1000 / 60) % 60
+   let seconds = Math.floor(gap / 1000) % 60
+
+   if (gap < 0){
+      
+   }
+
+      document.getElementById('d').innerText = days
+   document.getElementById('h').innerText = hours
+   document.getElementById('m').innerText = minutes
+   document.getElementById('s').innerText = seconds
+
+   console.log(days);
+}
+
+setInterval(counts, 1000)
